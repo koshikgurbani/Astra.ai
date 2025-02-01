ade8fcfba5aaeb0ad6fe7930d7d4d9be308a6552
Koshik <koshikgurbani@gmail.com>
2025-02-02 01:12:23 +0530

    Built Hero and Header component for the landing page

--- astra.ai/app/provider.jsx
+++ astra.ai/app/provider.jsx
@@ -1,17 +1,24 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import Header from '@/components/custom/Header'
+import { MessagesContext } from '@/context/MessagesContext'
 
 function Provider({ children }) {
+    const [messages, setMessages] = useState()
     return (
         <div>
-            <NextThemesProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange>
-                {children}
-            </NextThemesProvider>
+            <MessagesContext.Provider value={{ messages, setMessages }}>
+                <NextThemesProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Header />
+                    {children}
+                </NextThemesProvider>
+            </MessagesContext.Provider>
         </div>
     )
 }
