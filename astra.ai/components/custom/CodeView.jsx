00dec61ab893c5f2d466e298bd2bba4b73b469ed
Koshik <koshikgurbani@gmail.com>
2025-02-07 01:50:23 +0530

    added sandpack component for code editor and preview. Also added some default files and dependencies

--- astra.ai/components/custom/CodeView.jsx
+++ astra.ai/components/custom/CodeView.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
+import Lookup from '@/data/Lookup';
 
 function CodeView() {
-  console.log("inside code view")
+  const [activeTab, setActiveTab] = React.useState('code');
+  const [files,setFiles] =useState(Lookup?.DEFAULT_FILE)
   return (
-    <div>CodeView</div>
+    <div>
+      <div className='bg-[#181818] w-full p-2 border'>
+        <div className='flex items-center flex-wrap shrink-0
+         bg-black p-1 justify-center rounded-full
+          w-[140px] gap-3'>
+          <h2
+            onClick={() => setActiveTab('code')}
+            className={`text-sm cursor-pointer 
+            ${activeTab === 'code' && 'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full'}`}>
+            Code</h2>
+          <h2
+            onClick={() => setActiveTab('preview')}
+            className={`text-sm cursor-pointer 
+            ${activeTab === 'preview' && 'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full'}`}>
+            Preview</h2>
+        </div>
+      </div>
+      <SandpackProvider 
+      files={files}
+      template="react" theme={'dark'}
+      customSetup={{
+        dependencies: {
+          ...Lookup.DEPENDENCY
+        }
+      }}
+      options={{
+        externalResources: ['https://cdn.tailwindcss.com']
+      }}
+      >
+        <SandpackLayout>
+          {activeTab === 'code' && <>
+          <SandpackFileExplorer style={{ height: "80vh" }} />
+          <SandpackCodeEditor style={{ height: "80vh" }} />
+          </>}
+          {activeTab === 'preview' && <>
+          <SandpackPreview style={{ height: "80vh" }} showNavigator={true} />
+          </>}
+        </SandpackLayout>
+      </SandpackProvider>
+    </div>
   )
 }
 
