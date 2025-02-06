270996fed9dd6f045c7dcaa88888bc52df3ece70
Koshik <koshikgurbani@gmail.com>
2025-02-07 00:21:00 +0530

    added gemini template in config added ai responses in the cvhat view. Also added update messages function in convex to update the messages list in the workspace table after generating the ai response.

--- astra.ai/components/custom/ChatView.jsx
+++ astra.ai/components/custom/ChatView.jsx
@@ -2,56 +2,117 @@
 
 import { MessagesContext } from '@/context/MessagesContext';
 import { api } from '@/convex/_generated/api';
-import { useConvex } from 'convex/react';
+import { useConvex, useMutation } from 'convex/react';
 import { useParams } from 'next/navigation';
 import React, { useContext, useEffect, useState } from 'react'
 import Colors from '@/data/Colors';
 import { UserDetailContext } from '@/context/UserDetailContext';
 import Image from 'next/image';
-import { ArrowRight, Link } from 'lucide-react';
+import { ArrowRight, Link, Loader2Icon } from 'lucide-react';
 import Lookup from '@/data/Lookup';
+import axios from 'axios';
+import Prompt from '@/data/Prompt';
+import ReactMarkdown from 'react-markdown';
 
 function ChatView() {
     const { id } = useParams();
     const convex = useConvex();
-    const {userDetail,setUserDetail} = useContext(UserDetailContext);
+    const { userDetail, setUserDetail } = useContext(UserDetailContext);
     const { messages, setMessages } = useContext(MessagesContext);
-    const[userInput,setUserInput] =useState();
+    const [userInput, setUserInput] = useState();
+    const [loading, setLoading] = useState(false);
+    const UpdateMessages = useMutation(api.workspace.UpdateMessages);
+
     console.log("messages in the beginning of chat view ", messages)
     useEffect(() => {
-            console.log('Messages in chat view before fetching',messages)
-            id && GetWorkspaceData();
-        },[id])
-        
-        /* Used to Get Workspace data using Workspace ID */
-        const GetWorkspaceData = async () => {
-            const result = await convex.query(api.workspace.GetWorkspace, {
-                workspaceId: id
-            });
-            
-            console.log("getworkspace result in chat view",result);
-            if (result?.messages) {
-                console.log("result in chat view",result)
-                setMessages(result.messages);
-            } 
+        console.log('Messages in chat view before fetching', messages)
+        id && GetWorkspaceData();
+    }, [id])
+
+    /* Used to Get Workspace data using Workspace ID */
+    const GetWorkspaceData = async () => {
+        const result = await convex.query(api.workspace.GetWorkspace, {
+            workspaceId: id
+        });
+
+        console.log("getworkspace result in chat view", result);
+        if (result?.messages) {
+            console.log("result in chat view", result)
+            setMessages(result.messages);
         }
-        
-    console.log('Messages in chat view after fetching',messages)
+    }
+
+
+    console.log('Messages in chat view after fetching', messages)
+
+    useEffect(() => {
+        if (messages?.length > 0) {
+            const role = messages[messages?.length - 1].role;
+
+            if (role == 'user') {
+                GetAiResponse();
+            }
+        }
+    }, [messages])
+
+    const GetAiResponse = async () => {
+        setLoading(true);
+        const PROMPT = JSON.stringify(messages) + Prompt.CHAT_PROMPT;
+        const result = await axios.post('/api/ai-chat', {
+            prompt: PROMPT
+        })
+        console.log("ai prompt in chat view", PROMPT);
+        console.log("api response result in chat view", result.data.result);
+
+        const aiResp = {
+            role: 'ai',
+            content: result.data.result
+        }
+
+        setMessages(prev => [...prev, aiResp])
+
+        await UpdateMessages({
+            workspaceId: id,
+            messages: [...messages, aiResp]
+        })
+
+        setLoading(false);
+    }
+
+
+    const onGenerate = async (input) => {
+        const message = {
+            role: 'user',
+            content: input
+        }
+        setMessages(prev => [...prev, message]);
+        setUserInput('');
+    }
+
     return (
         <div className='relative h-[85vh] flex flex-col'>
-            <div className='flex-1 overflow-y-hidden'>
-                {messages?.map((msg,index) => (
-                     <div key={index}
-                     className='p-3 rounded-lg mb-2 flex gap-2 items-start'
-                     style={{backgroundColor: Colors.BACKGROUND}}
-                     >
-                        {msg?.role=='user'&&
-                        <Image src={userDetail?.picture} alt='userImage'
-                        width={35} height={35} className='rounded-full'/>}
-                        <h2>{msg.content}</h2>
+            <div className='flex-1 overflow-y-scroll scrollbar-hide'>
+                {messages?.map((msg, index) => (
+                    <div key={index}
+                        className='p-3 rounded-lg mb-2 flex gap-2 items-center leading-7'
+                        style={{ backgroundColor: Colors.BACKGROUND }}
+                    >
+                        {msg?.role == 'user' &&
+                            <Image src={userDetail?.picture} alt='userImage'
+                                width={35} height={35} className='rounded-full' />}
+                        {/* <h2>{msg.content}</h2> */}
+                        <ReactMarkdown className='flex flex-col'>{msg.content}</ReactMarkdown>
                     </div>
                 ))}
+                {loading && <div className='p-3 rounded-lg mb-2 flex gap-2 items-center'
+                    style={{ backgroundColor: Colors.BACKGROUND }}
+                >
+                    <Loader2Icon className='animate-spin' />
+                    <h2>Generating response...</h2>
+                </div>}
             </div>
+
+
             {/* INPUT SECTION */}
             <div className='p-5 border rounded-xl max-w-xl w-full mt-3'
                 style={{
@@ -59,6 +120,7 @@ function ChatView() {
                 }}>
                 <div className='flex gap-2'>
                     <textarea placeholder={Lookup.INPUT_PLACEHOLDER}
+                        value={userInput}
                         onChange={(event) => setUserInput(event.target.value)}
                         className='outline-none bg-transparent w-full h-32 max-h-56 resize-none' />
                     {userInput && <ArrowRight
